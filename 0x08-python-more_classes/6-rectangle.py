f3ef59acad90fd8d2dc07a53310cce53a8b6e6b9
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-10-05 01:01:00 +0000

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -95,4 +95,4 @@ class Rectangle:
         """
         if self.__height == 0 or self.__width == 0:
             return 0
-        return 2 * (self.__width + self.__height
+        return 2 * (self.__width + self.__height)
