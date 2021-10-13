05cacd7acdd369e0b7e3b4a478b1799ae18fcc48
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-10-13 11:28:40 +0000

    Update 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""Module 9-add_item.
-Adds all arguments to a Python list,
-and then save them to a file.
-"""
+'''task 9 module'''
 
-import sys
-import json
-import os.path
 
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+class Student:
+    '''student class'''
+    def __init__(self, first_name, last_name, age):
+        '''Initialization of the student object'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-my_file = 'add_item.json'
-
-my_list = []
-
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
-
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
-
-save_to_json_file(my_list, my_file)
+    def to_json(self):
+        '''return a dictionary representation of a Student instance'''
+        return self.__dict__
