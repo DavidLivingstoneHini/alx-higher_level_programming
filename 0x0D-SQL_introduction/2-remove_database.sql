25f38922312d5df61e31b2b566800fb239f0bf2d
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-11-18 01:24:16 +0000

    Completed

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes the database hbtn_0c_0 in your MySQL server.
+-- delete/drop database
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- lists all the tables of a database in your MySQL server
+-- list tables in database mysql
+SHOW TABLES;
